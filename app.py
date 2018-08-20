72d79ae0985bb4478fa89afe59c547f079107065
hanksterhan <henryhan62@gmail.com>
2018-08-20 19:02:09 -0400

    created a scatterplot dashboard with hover capabilities

--- app.py
+++ app.py
@@ -1,43 +1,45 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
 
 app = dash.Dash()
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+df = pd.read_csv(
+    'https://gist.githubusercontent.com/chriddyp/' +
+    '5d1ea79569ed194d432e56108a04d188/raw/' +
+    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
+    'gdp-life-exp-2007.csv')
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+app.layout = html.Div([
     dcc.Graph(
-        id='Graph1',
+        id='life-exp-vs-gdp',
         figure={
             'data': [
-                {'x': [1,2,3], 'y': [4,1,2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1,2,3], 'y': [2,4,5], 'type': 'bar', 'name': 'Montreal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['life expectancy'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.8,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
+            ], # this is one big list comprehension
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                yaxis={'title': 'Life Expectancy'},
+                margin={'l': 40, 'b':40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
